49378a6778af73d1dc8214d021d4b342ee90e043
Aleksandr Zelenin <aleksandr@zelenin.me>
2015-05-18 12:24:11 +0300

    add migration

---
+++ migrations/m150518_072330_alterRequestLogTable.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Migration;
+use yii\db\Schema;
+use Zelenin\yii\modules\RequestLog\models\RequestLog;
+
+class m150518_072330_alterRequestLogTable extends Migration
+{
+    public function safeUp()
+    {
+        $this->alterColumn(RequestLog::tableName(), 'params', Schema::TYPE_TEXT . ' not null');
+    }
+
+    public function safeDown()
+    {
+        $this->alterColumn(RequestLog::tableName(), 'params', Schema::TYPE_STRING . ' not null');
+        return true;
+    }
+}
